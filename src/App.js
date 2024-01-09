0383abcd707ea6d335d87205512aa991861b2d23
DeveloperYasmin <banuyasmin96@gmail.com>
2024-01-09 13:44:09 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
 import './App.css';
 import { Provider } from 'react-redux';
-
+import ReactDOM from "react-dom/client";
 import Body from './components/Body';
 import Head from './components/Head';
 import appStore from './utils/appStore';
-import {RouterProvider, createHashRouter } from 'react-router-dom';
+import {RouterProvider, createHashRouter, Outlet } from 'react-router-dom';
 import MainContainer from './components/MainContainer';
 import WatchPage from './components/WatchPage';
-
+function App() {
+  const AppLayout =() =>{
+  return (
+    <Provider store={appStore}>
+     <div>
+       <Head/>
+       <Outlet/>
+           </div>
+           </Provider>
+);
+  }
+  
 const appRouter=createHashRouter([{
   path:"/",
-  element:<Body/>,
+  element:<AppLayout/>,
   children:[
     {
       path:"/",
-      element:<MainContainer/>,
+      element:[<Body/>,<MainContainer/>]
     },
     
     {
       path:"watch",
       element:<WatchPage/>
     }
-  ]}])
-
-function App() {
-  return (
-   <Provider store={appStore}>
-
-    <div>
-      <Head/>
-      <RouterProvider router={appRouter}/>
-          </div>
-          </Provider>
-         
-
-  );
+  ]},])
+  ReactDOM.createRoot(document.getElementById("root")).render(<RouterProvider router={appRouter}/>)  
 }
 
 export default App;
